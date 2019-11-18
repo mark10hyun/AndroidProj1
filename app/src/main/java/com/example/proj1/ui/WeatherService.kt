00a63e170a79abd926b262860e97211fd3dbe689
lund <jl7271173@gmail.com>
2019-11-18 15:10:22 -0800

    checkin 7&8

--- app/src/main/java/com/example/proj1/ui/WeatherService.kt
+++ app/src/main/java/com/example/proj1/ui/WeatherService.kt
@@ -1,9 +1,4 @@
 package com.example.proj1.ui
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
+
 interface WeatherService {
-    @GET("data/2.5/weather?")
-    fun getCurrentWeatherData(@Query("lat")lat:String, @Query("Ion") Ion: String, @Query("APPID")  app_id: String): Call<WeatherResponse>
 }
